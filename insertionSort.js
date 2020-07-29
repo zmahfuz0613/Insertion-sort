6fd966c3a70f3e8bf9b1e0b9786ca44e17c9320e
Corey Gibson <cgibsonmmdev@gmail.com>
2020-07-29 08:34:00 -0400

    removed solution

--- insertionSort.js
+++ insertionSort.js
@@ -1,14 +1,13 @@
 function insertionSort(arr) {
-  for (var i = 1; i < arr.length; i++) {
-    let currentVal = arr[i];
-    for (var k = i - 1; k >= 0 && arr[k] > currentVal; k--) {
-      arr[k + 1] = arr[k];
-    }
-    arr[k + 1] = currentVal;
-  }
+  // 1. loop though arr starting at index 1
+  // 2. save current value to a var
+  // 3. compare current value to the value before if
+  // 4. if current value is less then current value index - 1 swap places
+  // 5. continue to compare current value to to all items that are in the array before it and swap there places
+  // 6. if currentVal is greater then your current comparison it should stop looping
 }
 
-let arr = [2, 1, 5, 3];
+let arr = [2, 1, 5, 3, 6, -1, 12];
 insertionSort(arr);
 
 console.log(arr);
